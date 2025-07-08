da01051763d04193e4347fb5740237e765f96612
Sai Main <saimain.primary@gmail.com>
2025-07-08 23:15:20 +0630

    init

---
+++ src/db/schema.js
@@ -0,0 +1,13 @@
+import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
+
+
+export const favoritesTable = pgTable("favorites",{
+    id: serial('id').primaryKey(),
+    userId : text('user_id').notNull(),
+    receiptId: integer('receipt_id').notNull(),
+    title: text('title').notNull(),
+    image: text('image'),
+    cookTime: text('cook_time'),
+    servings: text('servings'),
+    createdAt: timestamp('created_at').defaultNow(),
+})
